0d482c496a13af42f77cff825b8352118ae16a8e
Satha <sathanan.jeyakumar@ksb-sg.ch>
2022-11-13 23:18:32 +0100

    Augaben

--- M320/Aufgabe_3/Program.cs
+++ M320/Aufgabe_3/Program.cs
@@ -1,10 +1,55 @@
 ﻿namespace Aufgabe_3
 {
-    internal class Program
+    internal class calculator
     {
         static void Main(string[] args)
+
         {
-            Console.WriteLine("Hello, World!");
+            int a;
+            int b;
+            int ergebnis;
+            String eingabe;
+
+
+            Console.WriteLine("Geben Sie die erste Zahl ein: ");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Geben Sie die zweite Zahl ein: ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Geben sie den Operator ein: ");
+            eingabe = Convert.ToString(Console.ReadLine());
+
+            if (eingabe == "+")
+            {
+                ergebnis = a + b;
+
+                Console.WriteLine("Das Ergbenis ist: " + ergebnis);
+            }
+
+            else if (eingabe == "-")
+            {
+                ergebnis = a - b;
+
+                Console.WriteLine("Das Ergbenis ist: " + ergebnis);
+            }
+
+            else if (eingabe == "*")
+            {
+
+
+                Console.WriteLine("Das Ergbenis ist: " + a * b);
+            }
+
+            else if (eingabe == "/")
+            {
+
+                Console.WriteLine("Das Ergbenis ist: " + a / b);
+            }
+            else
+            {
+                System.Environment.Exit(0);
+            }
         }
     }
 }
